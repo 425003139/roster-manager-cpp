cc53030c4b182fb34eb9bdf254090944cbc7e8e1
425003139 <425003139@ntc.edu.ph>
2026-09-07 17:11:03 +0800

    Add function declaration in include/function.h

--- include/functions.h
+++ include/functions.h
@@ -1,1 +1,19 @@
-// TODO: Function declaration in this file
+#pragma once
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <string>
+
+struct Student {
+    int id;
+    std::string name;
+};
+
+int findStudentById(const Student roster[], int count, int targetId, int &comparisons);
+bool addStudent(Student roster[], int &count, int id, const std::string &name);
+bool removeStudentById(Student roster[], int &count, int targetId);
+bool safeGetRecord(const Student roster[], int count, int index, Student &outStudent);
+void printRoster(const Student roster[], int count);
+
+#endif // FUNCTIONS_H
